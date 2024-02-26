5c64c2bdd2ed2194276ef56f3fdebfa420f198ca
Кристина <dubininivan1990@gmail.com>
2024-02-26 16:57:42 +0400

    Выполнено задание

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,27 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
+
+        UserDaoJDBCImpl userDaoJDBC = new UserDaoJDBCImpl();
+//        userDaoJDBC.saveUser("BORIS", "Borisov", (byte) 20);
+//        userDaoJDBC.saveUser("Ivan", "Ivanov", (byte) 20);
+//        userDaoJDBC.saveUser("Nikolay", "Nikolaev", (byte) 20);
+//        userDaoJDBC.saveUser("Petr", "Petrov", (byte) 20);
+
+//        userDaoJDBC.removeUserById(12);
+
+        userDaoJDBC.cleanUsersTable();
+
+
+
+
     }
 }
--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,35 +1,89 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
-
+import jm.task.core.jdbc.util.Util;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
+
+    private final Connection connection = Util.getConnection();
+
     public UserDaoJDBCImpl() {
 
     }
 
     public void createUsersTable() {
-
+        try (Statement statement = connection.createStatement()) {
+            statement.executeUpdate("CREATE TABLE IF NOT EXISTS mynewdb.test1 " +
+                    "(id INT NOT NULL AUTO_INCREMENT, name VARCHAR(30), lastname VARCHAR(30)," +
+                    "age TINYINT UNSIGNED, PRIMARY KEY (id))");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void dropUsersTable() {
-
+        try (Statement statement = connection.createStatement()) {
+            statement.executeUpdate("DROP TABLE IF EXISTS mynewdb.test1");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void saveUser(String name, String lastName, byte age) {
-
+        try (PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO mynewdb.test1 " +
+                "(name, lastname, age) VALUES (?,?,?)");) {
+            preparedStatement.setString(1, name);
+            preparedStatement.setString(2, lastName);
+            preparedStatement.setString(3, String.valueOf(age));
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void removeUserById(long id) {
-
+        try (PreparedStatement statement = connection.prepareStatement("DELETE FROM mynewdb.test1 WHERE id = ?")) {
+            statement.setLong(1, id);
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public List<User> getAllUsers() {
-        return null;
+
+        List<User> userList = new ArrayList<>();
+
+        try (Statement statement = connection.createStatement()) {
+            ResultSet resultSet = statement.executeQuery("SELECT id, name, lastName, age FROM mynewdb.test1");
+
+            while (resultSet.next()) {
+                User user = new User();
+                user.setId(resultSet.getLong("id"));
+                user.setName(resultSet.getString("name"));
+                user.setLastName(resultSet.getString("lastName"));
+                user.setAge(resultSet.getByte("age"));
+                userList.add(user);
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return userList;
     }
 
     public void cleanUsersTable() {
 
+        try(Statement statement = connection.createStatement()) {
+            statement.executeUpdate("TRUNCATE mynewdb.test1");
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
+
